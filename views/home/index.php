2a20492496d8bac38d3b83aaf448fe530a60f506
Alex Iriarte <iriarte.alex77@gmail.com>
2022-08-17 13:31:17 -0300

    Converted to php

---
+++ views/home/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../resources/css/styles.css" />
+    <!---Bootstrap css--->
+    <link rel="stylesheet" href="../../resources/css/bootstrap.min.css">
+    <title>Homepage Ladrillo</title>
+</head>
+<body>
+    <!-- HEADER -->
+    <?php
+    include "header.inc";
+    ?>
+
+    <!-- NAVBAR -->
+    <?php
+    include "navbar.inc";
+    ?>
+
+    <!-- CONTENT -->
+    <?php
+    include "contentHome.inc";
+    ?>
+
+    <!-- CONTACT -->
+    <?php
+    include "contact.inc";
+    ?>
+
+    <!-- FOOTER -->
+    <?php
+    include "footer.inc";
+    ?>
+
+    <!---Font awesome--->
+    <script src="https://kit.fontawesome.com/cc72c2879b.js" crossorigin="anonymous"></script>
+    <!---Bootstrap js--->
+    <script src="../../resources/js/bootstrap.bundle.min.js"></script>
+     <!-- CDN Link to SB Forms Scripts -->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+
+</body>
+</html>
