94d8877d88e80e25e9b8f135d31c63f2e1741097
Matias Cabrera <matgcabrera@gmail.com>
2022-10-19 17:23:56 -0300

    reordered stylesheets priorities

--- views/home/index.php
+++ views/home/index.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../resources/css/styles.css" />
-    <link rel="stylesheet" href="../../resources/css/anim.css" />
     <!---Bootstrap css--->
     <link rel="stylesheet" href="../../resources/css/bootstrap.min.css">
     <!---VideoJS css--->
     <link href="../../resources/css/video-js.min.css" rel="stylesheet" />
     <!---Legacy VideoJS for compatibility--->
     <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
+    <link rel="stylesheet" href="../../resources/css/styles.css" />
+    <link rel="stylesheet" href="../../resources/css/anim.css" />
 
     <title>Homepage Ladrillo</title>
 </head>
