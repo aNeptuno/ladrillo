31b00a40289923e40ff09c27df1c9ca2387056a4
Matias Cabrera <matgcabrera@gmail.com>
2022-10-23 19:17:31 -0300

    new animations

--- resources/js/anim.js
+++ resources/js/anim.js
@@ -16,4 +16,25 @@ function reveal() {
     }
 }
 
+
+function revealHorizontal() {
+    //select all elements with class .reveal
+    var reveals = document.querySelectorAll(".reveal-horizontal"); 
+    //catch lenght that has been scrolled
+    for (var i = 0; i < reveals.length; i++) { 
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        //define how many pixels from the top of screen to trigger animation
+        var elementVisible = 150;
+        //conditions to trigger animation
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+
 window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", revealHorizontal);
